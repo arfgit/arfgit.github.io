aea4ae3ba3816a5e6bc2ba3483761378055614a8
Anthony Feliz <47192556+arfgit@users.noreply.github.com>
2022-09-13 21:39:38 -0400

    added second sketch

--- src/p5-one.js
+++ src/p5-one.js
@@ -1,63 +1,54 @@
-// let sketchOne = function (p) {
-//   p.setup = () => {
-//     p.createCanvas(500, 500);
-//     p.background(0);
-//   };
+//sketch one
+let sketchOne = function (p) {
+  let rot = 0;
+  p.setup = () => {
+    p.createCanvas(500, 500);
+    p.stroke(189, 29, 2);
+    p.background(0);
+    p.strokeWeight(0.5);
+  };
 
-//   p.draw = () => {
-//     //strokeWeight(random(0.1,1))
-//     p.translate(width / 2, height / 2);
-//     p.rotate(rot * -PI);
+  p.draw = () => {
+    p.translate(p.width / 2, p.height / 2);
+    p.rotate(rot * p.PI);
+    p.translate(100, 0);
+    p.rotate(rot);
+    p.scale(p.noise(rot * p.TWO_PI));
+    p.line(60 / rot, 60 * rot, 0, 0);
 
-//     //scale(random(0,1))
-//     p.translate(100, 0);
-//     //rotate(rot);
-//     p.rotate(rot);
-//     p.scale(noise(rot * 360));
-//     p.line(60 / rot, 60 * rot, 0, 0);
+    rot = rot + -p.PI / 100;
 
-//     rot = rot + PI / 10;
+    if (p.frameCount === 3000) {
+      noLoop();
+    }
+  };
+};
+new p5(sketchOne, "sketch-one");
 
-//     if (frameCount === 1600) {
-//       noLoop();
-//     }
-//   };
-// };
-// new p5(sketchOne, "sketch-one");
+//sketch two
+let sketchTwo = function (p) {
+  p.setup = () => {
+    p.createCanvas(500, 500);
+    p.angleMode(p.DEGREES);
+    p.rectMode(p.CENTER);
+  };
 
-// let sketchTwo = function (p) {
-//   p.setup = () => {
-//     p.createCanvas(500, 500);
-//   };
-
-//   p.draw = () => {
-//     p.background(0);
-//     p.ellipse(mouseX, mouseY, 50);
-//   };
-// };
-// new p5(sketchTwo, "sketch-two");
-let rot = 0;
-function setup() {
-  let canvas = createCanvas(500, 500);
-  canvas.parent("sketch-one");
-  // stroke(255, 100);
-  stroke(189, 29, 2);
-  strokeWeight(0.5);
-  background(0);
-}
-
-function draw() {
-  translate(width / 2, height / 2);
-  rotate(rot * PI);
-  translate(100, 0);
-  // rotate(rot);
-  rotate(rot);
-  scale(noise(rot * 360));
-  line(60 / rot, 60 * rot, 0, 0);
-
-  rot = rot + -PI / 100;
-
-  if (frameCount === 3000) {
-    noLoop();
-  }
-}
+  p.draw = () => {
+    p.background(18, 18, 18);
+    p.translate(p.width / 2, p.height / 2); //center
+    p.noFill();
+    p.stroke(255);
+    for (let i = 0; i < 100; i++) {
+      p.push();
+      p.rotate(p.sin(p.frameCount + i) * 100);
+      p.rotate(p.noise(i * 3));
+      let r = p.map(p.sin(p.frameCount), -1, 1, 50, 250);
+      let g = p.map(p.sin(p.frameCount / 2), -1, 1, 50, 250);
+      let b = p.map(p.sin(p.frameCount / 4), -1, 1, 50, 250);
+      p.stroke(r, g, b);
+      p.rect(0, 0, 300 - i, 50 - i * p.QUARTER_PI, 100 - i);
+      p.pop();
+    }
+  };
+};
+new p5(sketchTwo, "sketch-two");
